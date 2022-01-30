c5311589a22daf24fd164fb6a04cf3f18480da84
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2022-01-30 14:09:19 +0800

    Update minecraftpage.cpp

--- src/gui/pages/minecraftpage.cpp
+++ src/gui/pages/minecraftpage.cpp
@@ -9,7 +9,7 @@
 #include <gui/widgets/widgetutils.h>
 
 MinecraftPage::MinecraftPage(Config &config, QWidget *parent) : ConfigurationPage(config, parent) {
-    customMinecraftDir = new QCheckBox(QStringLiteral("Custom .minecraft Directory"));
+    customMinecraftDir = new QCheckBox(QStringLiteral("自定义 .minecraft 目录"));
     minecraftPathChooser = new FileChooser(QFileDialog::Directory);
 
     serverToJoin = new QLineEdit();
@@ -17,7 +17,7 @@ MinecraftPage::MinecraftPage(Config &config, QWidget *parent) : ConfigurationPag
 
     connect(serverToJoin, &QLineEdit::returnPressed, [this](){serverToJoin->clearFocus();});
 
-    joinServerOnLaunch = new QCheckBox(QStringLiteral("Join Server On Launch"));
+    joinServerOnLaunch = new QCheckBox(QStringLiteral("启动时加入服务器"));
 
     connect(joinServerOnLaunch, &QCheckBox::toggled, serverToJoin, &QLineEdit::setEnabled);
 
@@ -26,25 +26,25 @@ MinecraftPage::MinecraftPage(Config &config, QWidget *parent) : ConfigurationPag
     serverContainer->addWidget(joinServerOnLaunch, 0, Qt::AlignHCenter);
     serverContainer->addWidget(serverToJoin);
 
-    useNickHiderName = new QCheckBox(QStringLiteral("NickHider Name"));
+    useNickHiderName = new QCheckBox(QStringLiteral("NickHider 名称"));
     nickHiderName = new QLineEdit();
     nickHiderName->setPlaceholderText(QStringLiteral("You"));
 
-    useAutoggMessage = new QCheckBox(QStringLiteral("AutoGG Message"));
+    useAutoggMessage = new QCheckBox(QStringLiteral("AutoGG 消息"));
     autoggMessage = new QLineEdit();
     autoggMessage->setPlaceholderText(QStringLiteral("gg"));
 
-    useLevelHeadPrefix = new QCheckBox(QStringLiteral("LevelHead Prefix"));
+    useLevelHeadPrefix = new QCheckBox(QStringLiteral("水平头前缀"));
     levelHeadPrefix = new QLineEdit();
-    levelHeadPrefix->setPlaceholderText(QStringLiteral("Level: "));
+    levelHeadPrefix->setPlaceholderText(QStringLiteral("等级: "));
 
-    useLevelHeadNick = new QCheckBox(QStringLiteral("LevelHead Nick Level"));
+    useLevelHeadNick = new QCheckBox(QStringLiteral("LevelHead 尼克级别"));
     levelHeadNickLevel = new QLineEdit();
     levelHeadNickLevel->setPlaceholderText(QString::number(-1));
 
 
-    useCosmetics = new QCheckBox(QStringLiteral("Enable Cosmetics"));
-    unlockCosmetics = new QCheckBox(QStringLiteral("Unlock All Cosmetics"));
+    useCosmetics = new QCheckBox(QStringLiteral("启用化妆品"));
+    unlockCosmetics = new QCheckBox(QStringLiteral("解锁所有化妆品"));
 
     connect(useCosmetics, &QCheckBox::toggled, unlockCosmetics, &QCheckBox::setEnabled);
 
@@ -59,9 +59,9 @@ MinecraftPage::MinecraftPage(Config &config, QWidget *parent) : ConfigurationPag
 
     QHBoxLayout* windowResContainer = new QHBoxLayout();
     windowResContainer->setSpacing(30);
-    windowResContainer->addWidget(new QLabel(QStringLiteral("Window Width")));
+    windowResContainer->addWidget(new QLabel(QStringLiteral("窗口宽度")));
     windowResContainer->addWidget(windowWidth, 1);
-    windowResContainer->addWidget(new QLabel(QStringLiteral("Window Height")));
+    windowResContainer->addWidget(new QLabel(QStringLiteral("窗口高度")));
     windowResContainer->addWidget(windowHeight, 1);
         
     QVBoxLayout* mainLayout = new QVBoxLayout();
